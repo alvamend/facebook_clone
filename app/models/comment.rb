70bdcacbde519f3668ec92079c4214b1e4e23d06
Alvaro Mendoza <alvaromendozap99@gmail.com>
2024-05-10 15:16:02 -0500

    Created create method for Comments
    Added create method to comments, ran some tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    validates :content, presence: true
+    validates :content, presence: true, length: {minimum:1}
 
     belongs_to :user
     belongs_to :post
--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,11 @@ Rails.application.routes.draw do
       resources :posts
 
       # Like routes
-      get "like/:id", to: "likes#toggle_like"
+      get "/like/:id", to: "likes#toggle_like"
 
       # Comment routes
-      resources :comments
+      post "/comments/:post_id", to: "comments#create"
+      resources :comments, except: :create
 
       # Auth routes
       post "/auth/login", to: "auth#create"
